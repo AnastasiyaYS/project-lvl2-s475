93e0cb0245ebbc2dadd21ae31f7cd34018b8e45b
Anastasiya <Nastya199529@mail.ru>
2019-09-26 18:16:29 +0400

    make refactoring tests and function generateDiff

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,19 +4,24 @@ import genDiff from '../src';
 
 const buildPath = (fileName) => path.resolve(__dirname, `__fixtures__/${fileName}`);
 
-const table = [['JSON', buildPath('before.json'), buildPath('after.json'), 'Result.txt'],
-  ['YAML', buildPath('before.yml'), buildPath('after.yml'), 'Result.txt'],
-  ['INI', buildPath('before.ini'), buildPath('after.ini'), 'Result.txt']];
+const table = [['.json', 'tree'],
+  ['.yml', 'tree'],
+  ['.ini', 'tree'],
+  ['.json', 'plain'],
+  ['.yml', 'plain'],
+  ['.ini', 'plain'],
+  ['.json', 'json'],
+  ['.yml', 'json'],
+  ['.ini', 'json']];
 
-const makeTest = (format) => {
-  test.each(table)(
-    `compare %s files with ${format} format`,
-    (outputFormat, pathToFile1, pathToFile2, result) => {
-      expect(genDiff(pathToFile1, pathToFile2, `${format}`)).toEqual(fs.readFileSync(buildPath(`${format}${result}`), 'utf8'));
-    },
-  );
-};
-
-makeTest('tree');
-makeTest('plain');
-makeTest('json');
+test.each(table)(
+  'compare %s files with %s format',
+  (format, formatter) => {
+    const pathToFile1 = buildPath(`before${format}`);
+    const pathToFile2 = buildPath(`after${format}`);
+    const resultFileName = `${formatter}Result.txt`;
+    const actualValue = genDiff(pathToFile1, pathToFile2, formatter);
+    const expectedValue = fs.readFileSync(buildPath(resultFileName), 'utf8');
+    expect(actualValue).toEqual(expectedValue);
+  },
+);
--- src/index.js
+++ src/index.js
@@ -5,36 +5,32 @@ import parse from './utils/parsers';
 import formatProperties from './formatters/index';
 
 const generateDiff = (beforeObj, afterObj) => {
-  const resWithoutAdded = Object.entries(beforeObj).reduce((acc, [key, value]) => {
-    if (_.has(key, afterObj)) {
-      if (value instanceof Object && afterObj[key] instanceof Object) {
-        return [...acc, {
-          key, status: 'not changed', beforeValue: '', afterValue: '', children: generateDiff(value, afterObj[key]),
-        }];
+  const unitedKeys = _.union(Object.keys(afterObj), Object.keys(beforeObj));
+  return unitedKeys.map((key) => {
+    if (_.has(key, beforeObj) && _.has(key, afterObj)) {
+      if (beforeObj[key] instanceof Object && afterObj[key] instanceof Object) {
+        return {
+          key, status: 'not changed', beforeValue: '', afterValue: '', children: generateDiff(beforeObj[key], afterObj[key]),
+        };
       }
-      if (value === afterObj[key]) {
-        return [...acc, {
-          key, status: 'not changed', beforeValue: value, afterValue: value, children: '',
-        }];
+      if (beforeObj[key] === afterObj[key]) {
+        return {
+          key, status: 'not changed', beforeValue: beforeObj[key], afterValue: afterObj[key], children: '',
+        };
       }
-      return [...acc, {
-        key, status: 'changed', beforeValue: value, afterValue: afterObj[key], children: '',
-      }];
+      return {
+        key, status: 'changed', beforeValue: beforeObj[key], afterValue: afterObj[key], children: '',
+      };
     }
-    return [...acc, {
-      key, status: 'deleted', beforeValue: value, afterValue: '', children: '',
-    }];
-  }, []);
-
-  const res = Object.entries(afterObj).reduce((acc, [key, value]) => {
-    if (!_.has(key, beforeObj)) {
-      return [...acc, {
-        key, status: 'added', beforeValue: '', afterValue: value, children: '',
-      }];
+    if (_.has(key, beforeObj) && !_.has(key, afterObj)) {
+      return {
+        key, status: 'deleted', beforeValue: beforeObj[key], afterValue: '', children: '',
+      };
     }
-    return acc;
-  }, resWithoutAdded);
-  return res;
+    return {
+      key, status: 'added', beforeValue: '', afterValue: afterObj[key], children: '',
+    };
+  });
 };
 
 export default (firstConfig, secondConfig, format = 'tree') => {
